06e7ea39a0b5e55058fdc7f5c060344fc0af3f8e
caborryer <corraleshyulia@gmail.com>
2019-12-08 08:31:02 -0500

    primera parte rutas seguras

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,27 +1,42 @@
 const users = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const verifyToken = require("../utils/auth");
 const { encryptPassword, decryptPassword } = require("../utils/bycript");
+const config = require("../config/config");
 
 class userController {
   constructor() {}
+  //Registrar usuario
   async save(req, res, next) {
     try {
-      let { email, password } = req.body;
+      let { email, password, username } = req.body;
       password = await encryptPassword(password);
-      const user = new users({ email, password });
+      const user = new users({ email, password, username });
 
       user.save(err => {
         if (err)
           return res.status(400).json({ message: "Error on create user" });
 
-        res.status(200).json({
-          message: "The user has been registered",
-          user
+        const token = jwt.sign({ id: user._id }, config.secretAuthKey, {
+          expiresIn: 60 * 60 * 24
         });
+
+        res.status(200).json({ auth: true, token });
       });
     } catch (err) {
       console.log(err);
       res.status(500).json({ message: "Error: " + err.toString() });
     }
+  }
+
+  //Verificar JWT
+  async verify(req, res, next) {
+    const user = await User.findById(req.userId, { password: 0 });
+    if (!user) {
+      return res.status(404).send("No user found.");
+    }
+    res.status(200).json(user);
   }
 }
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
+const verifyToken = require("../utils/auth");
 
 const userControllers = require("../controllers/userControllers");
 
 const user = new userControllers();
 
 router.route("/signup").post(user.save);
+// router.route("/login").get(user.findOne);
+router.route("/user").get(verifyToken, user.verify);
 
 module.exports = router;
